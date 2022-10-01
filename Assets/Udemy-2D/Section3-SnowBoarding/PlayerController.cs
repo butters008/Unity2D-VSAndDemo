b8ece286e9661bd4794ab43dcf738aa3474cc6d5
Butters <k.butterfield08@gmail.com>
2022-10-01 17:34:01 -0500

    Finished Section 3!

--- Assets/Udemy-2D/Section3-SnowBoarding/PlayerController.cs
+++ Assets/Udemy-2D/Section3-SnowBoarding/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,22 +8,49 @@ public class PlayerController : MonoBehaviour
     //Declare 
     Rigidbody2D rb2d;
     [SerializeField] float force = 0;
+    SurfaceEffector2D surfaceEffector2D;
+    [SerializeField] float boostSpeed = 20.0f;
+    [SerializeField] float baseSpeed = 30.0f;
+    private bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D =  FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(canMove){
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void disableControls(){
+        canMove = false;
+    }
+
+  private void RespondToBoost()
+  {
+    //if we push up, then speed up
+    if (Input.GetKey(KeyCode.UpArrow)){
+        surfaceEffector2D.speed = boostSpeed;
+    }
+    else {
+        surfaceEffector2D.speed = baseSpeed;
+    }
+  }
+
+  void RotatePlayer(){
         if(Input.GetKey(KeyCode.LeftArrow)){
             RotationForce(force);
         }
         if(Input.GetKey(KeyCode.RightArrow)){
             RotationForce(-force);
-        }    
+        }
     }
 
     void RotationForce(float force){
