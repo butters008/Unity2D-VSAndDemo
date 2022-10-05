1cd3e29e880eb4370dfe5d415ff019550a8dfc63
Butters <k.butterfield08@gmail.com>
2022-10-05 17:27:26 -0500

    Getting Stuff done in section 4

--- Assets/Udemy-2D/Section4-QuizMaster/Scripts/QuestionsSO.cs
+++ Assets/Udemy-2D/Section4-QuizMaster/Scripts/QuestionsSO.cs
@@ -7,9 +7,19 @@ public class QuestionsSO : ScriptableObject
 {
     [TextArea(2, 6)] 
     [SerializeField] string quizQuestion = "Enter your question here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
     // This is a getter function
     public string getQuestion(){
         return quizQuestion;
+    }
+
+    public int getCorrectAnswerIndex(){
+        return correctAnswerIndex;
+    }
+
+    public string getAnswer(int index){
+        return answers[index];
     }
 }
