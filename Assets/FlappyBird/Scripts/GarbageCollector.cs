09ed290aa2566122b0568215d4993175278d14be
Butters <k.butterfield08@gmail.com>
2022-12-01 15:59:32 -0600

    Almost Done with Flappy bird clone
    After work, work on game dev

--- Assets/FlappyBird/Scripts/GarbageCollector.cs
+++ Assets/FlappyBird/Scripts/GarbageCollector.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class GarbageCollector : MonoBehaviour
 {
-  // [SerializeField] GameObject objectToDestroy;
-  [SerializeField] float xSpeed = 0.5f;
-
-
-  private void Update() {
-    moveGC();
-  }
-  // [SerializeField] GameObject objectToDestroy;
-  private void OnTriggerEnter2D(Collider2D other) {
+  // [SerializeField] GameObject objectToDestory;
+  private void OnTriggerEnter2D(Collider2D other)
+  {
     Debug.Log("Triggered in GC");
     Destroy(other.gameObject);
-  }
-  void moveGC(){
-    float xAmount = xSpeed * Time.deltaTime;  
-    transform.Translate(xAmount, 0, 0);
   }
 }
