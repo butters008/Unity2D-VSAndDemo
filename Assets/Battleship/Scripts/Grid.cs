927bef6be7e6caf13d12e3d2909be91d70a38e88
Butters <k.butterfield08@gmail.com>
2022-12-06 23:53:43 -0600

    Starting battleship game

---
+++ Assets/Battleship/Scripts/Grid.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+  [SerializeField] GameObject tile;
+  [SerializeField] int xLength;
+  [SerializeField] int yLength;
+  public void CreateGrid()
+  {
+
+  }
+}
