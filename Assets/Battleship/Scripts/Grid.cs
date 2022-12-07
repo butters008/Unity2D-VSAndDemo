603e9720a32d66ef14cd81463d957c3396cf36ac
Butters <k.butterfield08@gmail.com>
2022-12-07 00:08:39 -0600

    Starting on battleship game
    Working on getting the foundation down for game

--- Assets/Battleship/Scripts/Grid.cs
+++ Assets/Battleship/Scripts/Grid.cs
@@ -9,6 +9,20 @@ public class Grid : MonoBehaviour
   [SerializeField] int yLength;
   public void CreateGrid()
   {
-    //for
-  }
-}
+    //Ok, from Code Monkey and from the other guy this is what I have gathered, and will need to figure out
+    /*
+    Here is the list I think I need to complete in order to get the battleship game to work
+    1) Making the Grid
+      - From what I saw, I need to make a nested for loop, one for x and one for y
+      - Once the grid is complete, being able to click and read the cell value - this will also store the data for ships
+    */
+    //for(x = 0; x < xLength; x++){
+    // for(y = 0; y < yLength; y++){
+
+    //   }
+    // }
+
+
+  }//end of Grid
+
+}//End of class
