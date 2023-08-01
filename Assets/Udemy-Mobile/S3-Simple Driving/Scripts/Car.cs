88ed768a14a90cbebedc05d9a6318941dc565794
Butters <k.butterfield08@gmail.com>
2023-08-01 16:36:22 -0500

    Trying to get things sorted, coming up with ideas

--- Assets/Udemy-Mobile/S3-Simple Driving/Scripts/Car.cs
+++ Assets/Udemy-Mobile/S3-Simple Driving/Scripts/Car.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float speedIncrease;
-    [SerializeField] private float turnSpeed;
-    private int steerValue;
+  [SerializeField] private float speed;
+  [SerializeField] private float speedIncrease;
+  [SerializeField] private float turnSpeed;
+  private int steerValue;
 
-    // Update is called once per frame
-    void Update()
-    {
-        IncreaseSpeed();
-        transform.Rotate(0f, 0f, steerValue * turnSpeed * Time.deltaTime);
-        transform.Translate(0, speed * Time.deltaTime, 0);
-        Debug.Log(speed);
-    }
+  // Update is called once per frame
+  void Update()
+  {
+    IncreaseSpeed();
+    transform.Rotate(0f, 0f, steerValue * turnSpeed * Time.deltaTime);
+    transform.Translate(0, speed * Time.deltaTime, 0);
+    //Debug.Log(speed);
+  }
 
-    void IncreaseSpeed()
-    {
-        speed += speedIncrease * Time.deltaTime;
-    }
+  void IncreaseSpeed()
+  {
+    speed += speedIncrease * Time.deltaTime;
+  }
 
-    public void TurnCar(int value)
-    {
-        steerValue = value;
-    }
+  public void TurnCar(int value)
+  {
+    steerValue = value;
+    Debug.Log("Turning " + steerValue);
+  }
 }
